0a18e3574fc5a6d709e1257c0cfccd1b2637c7cf
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-06-17 14:38:30 +0800

    feat: initial-websocket-endpoint-api-update

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,11 +2,12 @@ import { Module } from "~/lib/modules/declarations";
 import { authDefinition } from "./auth";
 import { ServerHealth } from "./health";
 import { ServerLogging } from "./logging";
+import { testDefinition } from "./test";
 
 export const moduleDefinitions: Module[] = [
   ServerHealth,
   authDefinition,
   // postDefiniton,
-  // testDefinition,
+  testDefinition,
   ServerLogging,
 ];
